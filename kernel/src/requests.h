e638fcbb3ba0df5ed64746eada4033448612c814
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-04-26 18:20:42 -0500

    add requests (oops)

---
+++ kernel/src/requests.h
@@ -0,0 +1,41 @@
+#include <limine.h>
+__attribute__((used,
+               section(".limine_requests_"
+                       "start"))) static volatile LIMINE_REQUESTS_START_MARKER;
+__attribute__((
+    used, section(".limine_requests"))) static volatile LIMINE_BASE_REVISION(3);
+
+__attribute__((
+    used,
+    section(
+        ".limine_requests"))) static volatile struct limine_framebuffer_request
+    framebuffer_request = {.id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0};
+
+__attribute__((
+    used,
+    section(".limine_requests"))) static volatile struct limine_memmap_request
+    memmap_request = {.id = LIMINE_MEMMAP_REQUEST, .revision = 0};
+
+__attribute__((used, section(".limine_requests"))) static volatile struct
+    limine_executable_address_request addr_request = {
+        .id = LIMINE_EXECUTABLE_ADDRESS_REQUEST, .revision = 0};
+
+__attribute__((
+    used,
+    section(".limine_requests"))) static volatile struct limine_hhdm_request
+    hhdm_request = {.id = LIMINE_HHDM_REQUEST, .revision = 0};
+
+__attribute__((
+    used,
+    section(".limine_requests"))) static volatile struct limine_rsdp_request
+    rsdp_request = {.id = LIMINE_RSDP_REQUEST, .revision = 0};
+
+__attribute__((
+    used, section(".limine_requests"))) static volatile struct limine_mp_request
+    mp_request = {.id = LIMINE_MP_REQUEST, .revision = 0};
+
+__attribute__((
+    used,
+    section(
+        ".limine_requests_end"))) static volatile LIMINE_REQUESTS_END_MARKER;
+
