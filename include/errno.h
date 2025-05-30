32ad7972158ebf452838fcae207ed611ff8349b7
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-05-29 22:54:36 -0500

    errno implementation - gotta start returning errors

--- include/errno.h
+++ include/errno.h
@@ -1,127 +1,54 @@
-// stolen from
-//
-// https://kernel.googlesource.com/pub/scm/linux/kernel/git/nico/archive/+/v0.97/include/linux/errno.h
-//
-// thank you linux
+#pragma once
 
-#define EPERM 1            /* Operation not permitted */
-#define ENOENT 2           /* No such file or directory */
-#define ESRCH 3            /* No such process */
-#define EINTR 4            /* Interrupted system call */
-#define EIO 5              /* I/O error */
-#define ENXIO 6            /* No such device or address */
-#define E2BIG 7            /* Arg list too long */
-#define ENOEXEC 8          /* Exec format error */
-#define EBADF 9            /* Bad file number */
-#define ECHILD 10          /* No child processes */
-#define EAGAIN 11          /* Try again */
-#define ENOMEM 12          /* Out of memory */
-#define EACCES 13          /* Permission denied */
-#define EFAULT 14          /* Bad address */
-#define ENOTBLK 15         /* Block device required */
-#define EBUSY 16           /* Device or resource busy */
-#define EEXIST 17          /* File exists */
-#define EXDEV 18           /* Cross-device link */
-#define ENODEV 19          /* No such device */
-#define ENOTDIR 20         /* Not a directory */
-#define EISDIR 21          /* Is a directory */
-#define EINVAL 22          /* Invalid argument */
-#define ENFILE 23          /* File table overflow */
-#define EMFILE 24          /* Too many open files */
-#define ENOTTY 25          /* Not a typewriter */
-#define ETXTBSY 26         /* Text file busy */
-#define EFBIG 27           /* File too large */
-#define ENOSPC 28          /* No space left on device */
-#define ESPIPE 29          /* Illegal seek */
-#define EROFS 30           /* Read-only file system */
-#define EMLINK 31          /* Too many links */
-#define EPIPE 32           /* Broken pipe */
-#define EDOM 33            /* Math argument out of domain of func */
-#define ERANGE 34          /* Math result not representable */
-#define EDEADLK 35         /* Resource deadlock would occur */
-#define ENAMETOOLONG 36    /* File name too long */
-#define ENOLCK 37          /* No record locks available */
-#define ENOSYS 38          /* Function not implemented */
-#define ENOTEMPTY 39       /* Directory not empty */
-#define ELOOP 40           /* Too many symbolic links encountered */
-#define EWOULDBLOCK 41     /* Operation would block */
-#define ENOMSG 42          /* No message of desired type */
-#define EIDRM 43           /* Identifier removed */
-#define ECHRNG 44          /* Channel number out of range */
-#define EL2NSYNC 45        /* Level 2 not synchronized */
-#define EL3HLT 46          /* Level 3 halted */
-#define EL3RST 47          /* Level 3 reset */
-#define ELNRNG 48          /* Link number out of range */
-#define EUNATCH 49         /* Protocol driver not attached */
-#define ENOCSI 50          /* No CSI structure available */
-#define EL2HLT 51          /* Level 2 halted */
-#define EBADE 52           /* Invalid exchange */
-#define EBADR 53           /* Invalid request descriptor */
-#define EXFULL 54          /* Exchange full */
-#define ENOANO 55          /* No anode */
-#define EBADRQC 56         /* Invalid request code */
-#define EBADSLT 57         /* Invalid slot */
-#define EDEADLOCK 58       /* File locking deadlock error */
-#define EBFONT 59          /* Bad font file format */
-#define ENOSTR 60          /* Device not a stream */
-#define ENODATA 61         /* No data available */
-#define ETIME 62           /* Timer expired */
-#define ENOSR 63           /* Out of streams resources */
-#define ENONET 64          /* Machine is not on the network */
-#define ENOPKG 65          /* Package not installed */
-#define EREMOTE 66         /* Object is remote */
-#define ENOLINK 67         /* Link has been severed */
-#define EADV 68            /* Advertise error */
-#define ESRMNT 69          /* Srmount error */
-#define ECOMM 70           /* Communication error on send */
-#define EPROTO 71          /* Protocol error */
-#define EMULTIHOP 72       /* Multihop attempted */
-#define EDOTDOT 73         /* RFS specific error */
-#define EBADMSG 74         /* Not a data message */
-#define EOVERFLOW 75       /* Value too large for defined data type */
-#define ENOTUNIQ 76        /* Name not unique on network */
-#define EBADFD 77          /* File descriptor in bad state */
-#define EREMCHG 78         /* Remote address changed */
-#define ELIBACC 79         /* Can not access a needed shared library */
-#define ELIBBAD 80         /* Accessing a corrupted shared library */
-#define ELIBSCN 81         /* .lib section in a.out corrupted */
-#define ELIBMAX 82         /* Attempting to link in too many shared libraries */
-#define ELIBEXEC 83        /* Cannot exec a shared library directly */
-#define EILSEQ 84          /* Illegal byte sequence */
-#define ERESTART 85        /* Interrupted system call should be restarted */
-#define ESTRPIPE 86        /* Streams pipe error */
-#define EUSERS 87          /* Too many users */
-#define ENOTSOCK 88        /* Socket operation on non-socket */
-#define EDESTADDRREQ 89    /* Destination address required */
-#define EMSGSIZE 90        /* Message too long */
-#define EPROTOTYPE 91      /* Protocol wrong type for socket */
-#define ENOPROTOOPT 92     /* Protocol not available */
-#define EPROTONOSUPPORT 93 /* Protocol not supported */
-#define ESOCKTNOSUPPORT 94 /* Socket type not supported */
-#define EOPNOTSUPP 95      /* Operation not supported on transport endpoint */
-#define EPFNOSUPPORT 96    /* Protocol family not supported */
-#define EAFNOSUPPORT 97    /* Address family not supported by protocol */
-#define EADDRINUSE 98      /* Address already in use */
-#define EADDRNOTAVAIL 99   /* Cannot assign requested address */
-#define ENETDOWN 100       /* Network is down */
-#define ENETUNREACH 101    /* Network is unreachable */
-#define ENETRESET 102      /* Network dropped connection because of reset */
-#define ECONNABORTED 103   /* Software caused connection abort */
-#define ECONNRESET 104     /* Connection reset by peer */
-#define ENOBUFS 105        /* No buffer space available */
-#define EISCONN 106        /* Transport endpoint is already connected */
-#define ENOTCONN 107       /* Transport endpoint is not connected */
-#define ESHUTDOWN 108      /* Cannot send after transport endpoint shutdown */
-#define ETOOMANYREFS 109   /* Too many references: cannot splice */
-#define ETIMEDOUT 110      /* Connection timed out */
-#define ECONNREFUSED 111   /* Connection refused */
-#define EHOSTDOWN 112      /* Host is down */
-#define EHOSTUNREACH 113   /* No route to host */
-#define EALREADY 114       /* Operation already in progress */
-#define EINPROGRESS 115    /* Operation now in progress */
-#define ESTALE 116         /* Stale NFS file handle */
-#define EUCLEAN 117        /* Structure needs cleaning */
-#define ENOTNAM 118        /* Not a XENIX named type file */
-#define ENAVAIL 119        /* No XENIX semaphores available */
-#define EISNAM 120         /* Is a named type file */
-#define EREMOTEIO 121      /* Remote I/O error */
+enum errno {
+    ERR_OK = 0,        // Success
+    ERR_UNKNOWN = 1,   // Unknown or unspecified error
+    ERR_NO_MEM = 2,    // Out of memory
+    ERR_NO_DEV = 3,    // No such device
+    ERR_NO_ENT = 4,    // No such file or directory
+    ERR_EXIST = 5,     // File already exists
+    ERR_IO = 6,        // I/O error
+    ERR_NOT_DIR = 7,   // Not a directory
+    ERR_IS_DIR = 8,    // Is a directory
+    ERR_INVAL = 9,     // Invalid argument
+    ERR_PERM = 10,     // Permission denied
+    ERR_FAULT = 11,    // Bad address
+    ERR_BUSY = 12,     // Resource/device busy
+    ERR_AGAIN = 13,    // Try again later
+    ERR_NOT_IMPL = 14, // Not implemented
+    ERR_NOSPC = 15,    // No space left on device
+    ERR_OVERFLOW = 16, // Value too large
+
+    ERR_NO_INODE = 100,     // Inode not found
+    ERR_CORRUPT = 101,      // Filesystem corruption
+    ERR_SYMLINK_LOOP = 102, // Too many symlink levels
+
+};
+
+static inline const char *errno_to_str(enum errno err) {
+    switch (err) {
+    case ERR_OK: return "No error";
+    case ERR_UNKNOWN: return "Unknown error";
+    case ERR_NO_MEM: return "Out of memory";
+    case ERR_NO_DEV: return "No such device";
+    case ERR_NO_ENT: return "No such file or directory";
+    case ERR_EXIST: return "File already exists";
+    case ERR_IO: return "I/O error";
+    case ERR_NOT_DIR: return "Not a directory";
+    case ERR_IS_DIR: return "Is a directory";
+    case ERR_INVAL: return "Invalid argument";
+    case ERR_PERM: return "Permission denied";
+    case ERR_FAULT: return "Bad memory access";
+    case ERR_BUSY: return "Resource busy";
+    case ERR_AGAIN: return "Try again";
+    case ERR_NOT_IMPL: return "Not implemented";
+    case ERR_NOSPC: return "No space left";
+    case ERR_OVERFLOW: return "Value too large";
+
+    case ERR_NO_INODE: return "Inode not found";
+    case ERR_CORRUPT: return "Filesystem corruption";
+    case ERR_SYMLINK_LOOP: return "Symlink loop";
+
+    default: return "Unrecognized error code";
+    }
+}
