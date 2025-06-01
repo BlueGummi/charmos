789b9cc3cb4a2e35af2e4778801ff81a37b3662e
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-06-01 10:32:49 -0500

    [fix]: conditionally map the BAR0 size

--- kernel/devices/nvme/nvme.c
+++ kernel/devices/nvme/nvme.c
@@ -9,9 +9,28 @@
 
 struct nvme_device *nvme_discover_device(uint8_t bus, uint8_t slot,
                                          uint8_t func) {
-    uint32_t bar0 = pci_read(bus, slot, func, 0x10) & ~0xF;
 
-    void *mmio = vmm_map_phys(bar0, 4096 * 2); // two pages coz doorbell thingy
+    uint32_t original_bar0 = pci_read(bus, slot, func, 0x10);
+    bool is_io = original_bar0 & 1;
+
+    if (is_io) {
+        panic("doesnt look like mmio to me");
+    }
+
+    pci_write(bus, slot, func, 0x10, 0xFFFFFFFF);
+
+    uint32_t size_mask = pci_read(bus, slot, func, 0x10);
+
+    pci_write(bus, slot, func, 0x10, original_bar0);
+
+    uint32_t size = ~(size_mask & ~0xF) + 1; 
+
+    if (size == 0) {
+        k_panic("bar0 reports zero size ?");
+    }
+
+    uint32_t phys_addr = original_bar0 & ~0xF;
+    void *mmio = vmm_map_phys(phys_addr, size);
     struct nvme_regs *regs = (struct nvme_regs *) mmio;
     uint64_t cap = ((uint64_t) regs->cap_hi << 32) | regs->cap_lo;
     uint32_t version = regs->version;
@@ -52,6 +71,6 @@ struct generic_disk *nvme_create_generic(struct nvme_device *nvme) {
     d->read_sector = nvme_read_sector;
     d->write_sector = nvme_write_sector;
     d->print = nvme_print_wrapper;
-    d->type = G_NVME_DRIVE; 
+    d->type = G_NVME_DRIVE;
     return d;
 }
