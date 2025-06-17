fb4e7da09d19e8bb715a4fc82af678803f893236
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-06-17 13:48:37 -0500

    [xhci]: start work

---
+++ include/drivers/xhci.h
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#pragma once
+
+// 5.3: XHCI Capability Registers
+struct xhci_cap_regs {
+    uint8_t cap_length;
+    uint8_t reserved;
+    uint16_t hci_version;
+    uint32_t hcs_params1;
+    uint32_t hcs_params2;
+    uint32_t hcs_params3;
+    uint32_t hcc_params1;
+    uint32_t dboff;
+    uint32_t rtsoff;
+    uint32_t hcc_params2;
+} __attribute__((packed));
+
+// 5.4: XHCI Operational Registers
+struct xhci_op_regs {
+    uint32_t usbcmd;
+    uint32_t usbsts;
+    uint32_t pagesize;
+    uint32_t reserved[2];
+    uint32_t dnctrl;
+    uint64_t crcr;
+    uint32_t reserved2[4];
+    uint64_t dcbaap;
+    uint32_t config;
+} __attribute__((packed));
+
+struct xhci_trb { // Transfer Ring Buffer
+    uint32_t dword0;
+    uint32_t dword1;
+    uint32_t dword2;
+    uint32_t dword3;
+};
+
+struct xhci_ring {
+    struct xhci_trb *trbs;
+    uint64_t phys;
+    uint32_t enqueue_index;
+    uint8_t cycle;
+};
--- kernel/drivers/usb.c
+++ kernel/drivers/usb.c
@@ -1,32 +1,75 @@
 #include <asm.h>
 #include <console/printf.h>
+#include <drivers/xhci.h>
 #include <mem/alloc.h>
 #include <mem/pmm.h>
 #include <mem/vmm.h>
 #include <stdint.h>
+#include <string.h>
 
 void usb_init(uint8_t bus, uint8_t slot, uint8_t func) {
-
     uint32_t original_bar0 = pci_read(bus, slot, func, 0x10);
-
-    bool is_io = original_bar0 & 1;
-
-    if (is_io) {
-        k_panic("doesnt look like mmio to me");
-    }
+    if (original_bar0 & 1)
+        k_panic("BAR0 is not MMIO");
 
     pci_write(bus, slot, func, 0x10, 0xFFFFFFFF);
-
     uint32_t size_mask = pci_read(bus, slot, func, 0x10);
-
     pci_write(bus, slot, func, 0x10, original_bar0);
 
     uint32_t size = ~(size_mask & ~0xF) + 1;
-
-    if (size == 0) {
-        k_panic("bar0 reports zero size ?");
-    }
+    if (size == 0)
+        k_panic("BAR0 reports zero size");
 
     uint32_t phys_addr = original_bar0 & ~0xF;
     void *mmio = vmm_map_phys(phys_addr, size);
+
+    struct xhci_cap_regs *cap = (struct xhci_cap_regs *) mmio;
+    struct xhci_op_regs *op =
+        (struct xhci_op_regs *) ((uintptr_t) mmio + cap->cap_length);
+
+    uint32_t usbcmd = mmio_read_32(&op->usbcmd);
+    usbcmd &= ~1;
+    mmio_write_32(&op->usbcmd, usbcmd);
+    while ((mmio_read_32(&op->usbsts) & 1) == 0)
+        ;
+
+    usbcmd = mmio_read_32(&op->usbcmd);
+    usbcmd |= (1 << 1);
+    mmio_write_32(&op->usbcmd, usbcmd);
+    while (mmio_read_32(&op->usbcmd) & (1 << 1))
+        ;
+
+    mmio_write_32(&op->pagesize, 1); // 4096
+
+    struct xhci_ring *cmd_ring = kmalloc(sizeof(struct xhci_ring));
+
+    uint64_t ring_size = sizeof(struct xhci_trb) * 256;
+
+    cmd_ring->phys = (uint64_t) pmm_alloc_pages(ring_size / PAGE_SIZE, false);
+
+    cmd_ring->trbs = vmm_map_phys(cmd_ring->phys, ring_size);
+    memset(cmd_ring->trbs, 0, ring_size);
+
+    cmd_ring->enqueue_index = 0;
+    cmd_ring->cycle = 1;
+
+    mmio_write_32(&op->crcr, cmd_ring->phys | 1);
+
+    uint64_t dcbaa_size = 256 * (sizeof(uint64_t));
+    uint64_t dcbaa_phys =
+        (uint64_t) pmm_alloc_pages(dcbaa_size / PAGE_SIZE, false);
+
+    uint64_t *dcbaa = vmm_map_phys(dcbaa_phys, dcbaa_size);
+    memset(dcbaa, 0, dcbaa_size);
+
+    mmio_write_64(&op->dcbaap, dcbaa_phys);
+
+    usbcmd = mmio_read_32(&op->usbcmd);
+    usbcmd |= 1;
+    mmio_write_32(&op->usbcmd, usbcmd);
+
+    while (mmio_read_32(&op->usbsts) & 1)
+        ;
+
+    k_printf("XHCI controller initialized.\n");
 }
